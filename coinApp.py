bc4f2d951ff20e8cd8deb4c9a67c6d1687a3c72a
Tivadar Kamondy <43910197+tivadark@users.noreply.github.com>
2021-01-03 15:22:43 +0100

    Improved, final version
    The code has been refactored and improved

--- coinApp.py
+++ coinApp.py
@@ -1,124 +1,87 @@
-#Kamondy Tivadar - 2020
+#Kamondy Tivadar - 2020/21/1
 #Computer vision course - Szechenyi Istvan University of Gyor
-#erme recognition application
-
-#TODO LIST:
-#Ismeretlen meretu kor?
-#Erme szin
-#UI?
+#Coin recognition application
 
 import cv2
-import numpy as np
+#OpenCv konyvtar importalasa
 
-def erme_detektalasa():
-    erme_forint = cv2.imread('input_kepek/ermek.jpg', 1)
+forint_ermek  = {
+        "5 Ft": {
+            "ertek": 5,
+            "sugar": 37.5,
+        },
+        "10 Ft": {
+            "ertek": 10,
+            "sugar": 44.7,
+        },
+        "20 Ft": {
+            "ertek": 20,
+            "sugar": 51.2,
+        },
+        "50 Ft": {
+            "ertek": 50,
+            "sugar": 54.5,
+        },
+        "100 Ft": {
+            "ertek": 100,
+            "sugar": 40.7,
+        },
+        "200 Ft": {
+            "ertek": 200,
+            "sugar": 57.5,
+        },
+    }
 
-    szurkeranyalatos = cv2.cvtColor(erme_forint, cv2.COLOR_BGR2GRAY)
-    forraskep = cv2.medianBlur(szurkeranyalatos, 7)
+def erme_detektalasa(ermek):
+    kep = cv2.cvtColor(ermek, cv2.COLOR_BGR2GRAY) #a kep szurkearnyalatossa konvertalasa
+    kep = cv2.medianBlur(kep, 21) #a kep elhomalyositasa a kep aprobb adatainak eltorlese vegett
     
-    kor_alakzatok = cv2.HoughCircles( ##Hough kor transzformacio parameterezes
-        forraskep,
+    kor_alakzatok = cv2.HoughCircles(
+        kep,  # bemeneti kep
         cv2.HOUGH_GRADIENT,  # detektalas tipusa
         1,
         50,
         param1=100,
         param2=50,
-        minRadius=10,  # minimum sugar
-        maxRadius=380,  # maximum sugar
+        minRadius=5,  # minimalis sugar meret
+        maxRadius=350,  # max sugar
     )
 
-    ermek_masol = erme_forint.copy()
-
-    for felismert_kor_alakzatok in kor_alakzatok[0]:
+    #kor_alakzatok detektalasa es a zold korvonal kirajzolasa korulottuk
+    for felismert_kor_alakzatok in kor_alakzatok[0]: 
         x_koord, y_koord, detektalt_sugar = felismert_kor_alakzatok
-        detektalt_ermek = cv2.circle(
-            ermek_masol,
+        detektalt_ermek = cv2.circle( #a kor megrajzolasa a koordinatak es sugarak felhasznalasaval
+            ermek,
             (int(x_koord), int(y_koord)),
             int(detektalt_sugar),
             (0, 255, 0),
             4,
         )
 
-    cv2.imwrite("output_kepek/ermek_Hough.jpg", detektalt_ermek)
+    return kor_alakzatok[0],detektalt_ermek
 
-    return kor_alakzatok
+def osszeg_szamitas(ermek_adat,kuszobertek ,korvonalas_ermek):
+    adat = {} #exportalando szotar
+    for erme_adat in ermek_adat: #ermeken vegig iteralas
+        for k in forint_ermek : #vegig iteralas az ermek eloredefinialt adatain
+            if abs(erme_adat[2] - forint_ermek [k]["sugar"]) <= kuszobertek: #ha talalkozik a kuszobertekkel
+                if k in adat.keys(): #ha a kulcs nem talalhato akkor hozzon letre egy ujat
+                    adat[k] += 1
+                else:
+                    adat[k] = 1 #ermekhez hozzaadas
+                cv2.putText(korvonalas_ermek, str(forint_ermek [k]["ertek"]), (int(erme_adat[0]), int(erme_adat[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 4) #az adott ermere az ertek kiiratasa
+    return adat, korvonalas_ermek #adat es a kep visszaadasa mar az ertekekkel egyutt
 
-def osszeg_szamitas(): #TODO: szin hozzaadas
-    ermek = {
-        "5 Ft": {
-            "ertek": 5,
-            "sugar": 19.5,
-            "arany": 1,
-            "darab": 0,
-        },
-        "10 Ft": {
-            "ertek": 10,
-            "sugar": 21.5,
-            "arany": 1.2,
-            "darab": 0,
-        },
-        "20 Ft": {
-            "ertek": 20,
-            "sugar": 22.5,
-            "arany": 1.249,
-            "darab": 0,
-        },
-        "50 Ft": {
-            "ertek": 50,
-            "sugar": 25,
-            "arany": 1.33,
-            "darab": 0,
-        },
-        "100 Ft": {
-            "ertek": 100,
-            "sugar": 21,
-            "arany": 1.115,
-            "darab": 0,
-        },
-        "200 Ft": {
-            "ertek": 200,
-            "sugar": 30,
-            "arany": 1.4,
-            "darab": 0,
-        },
-    }
+if __name__ == "__main__":
+    kep = cv2.imread('input_kepek/ermek1.jpg')
 
-    kor_alakzatok = erme_detektalasa()
-    sugar = []
-    koordinatak = []
-
-    for felismert_kor_alakzatok in kor_alakzatok[0]:
-        x_koord, y_koord, detektalt_sugar = felismert_kor_alakzatok
-        sugar.append(detektalt_sugar)
-        koordinatak.append([x_koord, y_koord])
-
-    legkisebb = min(sugar)
-    kuszobertek = 0.0375
+    ermek_adat,ermek_detektalt_kep = erme_detektalasa(kep) #ermek detektalasa
+    cv2.imwrite("output_kepek/ermek_Hough.jpg",ermek_detektalt_kep)
+    adat,kep = osszeg_szamitas(ermek_adat,1.5,ermek_detektalt_kep) #az ermek osszegenek szamitasa
     teljes_osszeg = 0
-
-    korvonalas_ermek = cv2.imread('output_kepek/ermek_Hough.jpg', 1)
-    betutipus = cv2.FONT_HERSHEY_SIMPLEX
-
-    for erme in kor_alakzatok[0]:
-        arany_ell = erme[2] / legkisebb
-        koord_x = erme[0]
-        koord_y = erme[1]
-        for forint in ermek:
-            ertek = ermek[forint]['ertek']
-            if abs(arany_ell - ermek[forint]['arany']) <= kuszobertek:
-                ermek[forint]['darab'] += 1
-                teljes_osszeg += ermek[forint]['ertek']
-                cv2.putText(korvonalas_ermek, str(ertek), (int(koord_x), int(koord_y)), betutipus, 1,
-                            (0, 0, 0), 4)
-
-    print(f"Az ermek teljes osszege {teljes_osszeg} Ft")
-    for forint in ermek:
-        darabszam = ermek[forint]['darab']
-        print(f"{darabszam}x = {forint}")
-
-
-    cv2.imwrite("output_kepek/ermek_daraertek_korvonal.jpg", korvonalas_ermek)
-
-
-if __name__ == "__main__": ##az osszeg kiszamolasa a __name__ guardot kovetoen
-    osszeg_szamitas()
+    
+    for a in adat:
+        print(a,": x", adat[a])
+        teljes_osszeg += adat[a] * int(a.split(" ")[0])
+    print("Teljes osszeg: ", teljes_osszeg)
+    cv2.imwrite("output_kepek/ermek_darabertek_korvonal.jpg",kep)
